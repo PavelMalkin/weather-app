1c06012593b3c1e9dc75ed218f9f178dfdb2b14c
PavelMalkin <skystresss@gmail.com>
2020-04-06 22:26:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs')
 var mysql = require('mysql');
-var request = require('request');
+// var request = require('request');
 var moment = require('moment');
 // var schedule = require('node-schedule');
 var port = process.env.PORT || 3000;
@@ -15,98 +15,65 @@ let axios = require('axios');
 //   console.log('The answer to life, the universe, and everything!' + d );
 // });
 
-
-let con = mysql.createConnection({
-    host: "localhost",
-    user: "User1",
-    password: "admintest",
-    database: 'mydb'
-});
+// let sql = 'INSERT INTO weather1 (Name, Date, Temperature, WindDirection, WindSpeed, WindGust) VALUES ?';
 //
-// let currentweather = {
-//     'method': 'GET',
-//     'url': 'http://api.openweathermap.org/data/2.5/weather?q=Haifa,il&appid=82d3d463b96e530d4b14b76571e81d3a',
-//     'headers': {
-//     }
-// };
-
-function currentweather() {
-    axios.get('http://api.openweathermap.org/data/2.5/weather?q=Haifa,il&appid=82d3d463b96e530d4b14b76571e81d3a')
-        .then(res => {
-            let result = res.data;
-            let date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
-            let values = [['Current weather', date, result.main.temp - 273.15, result.wind.deg, result.wind.speed, result.wind.gust]];
-            con.query(sql, [values], function (err, result, fields) {
-                if (err) log += err;
-                console.log('Current weather: ' + result.affectedRows + date);
-                log += 'Current weather: ' + result.affectedRows + date + ';   ';
-            });
-
-        })
-        .catch(err => console.log('Error', err))
-}
-
-
-currentweather();
-
-//
-// let openweathermap = {
-//     'method': 'GET',
-//     'url': 'http://api.openweathermap.org/data/2.5/forecast?q=Haifa,il&appid=82d3d463b96e530d4b14b76571e81d3a'
-// };
-//
-let sql = 'INSERT INTO weather1 (Name, Date, Temperature, WindDirection, WindSpeed, WindGust) VALUES ?';
+// let con = mysql.createConnection({
+//     host: "localhost",
+//     user: "User1",
+//     password: "admintest",
+//     database: 'mydb'
+// });
 //
 // con.connect(function (err) {
 //     if (err) log += err + ';   ';
 //     console.log("Connected!");
 // });
 //
+// function currentweather() {
+//     axios.get('http://api.openweathermap.org/data/2.5/weather?q=Haifa,il&appid=82d3d463b96e530d4b14b76571e81d3a')
+//         .then(res => {
+//             let result = res.data;
+//             let date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+//             let values = [['Current weather', date, result.main.temp - 273.15, result.wind.deg, result.wind.speed, result.wind.gust]];
+//             con.query(sql, [values], function (err, result, fields) {
+//                 if (err) log += err;
+//                 console.log('Current weather: ' + result.affectedRows + date);
+//                 log += 'Current weather: ' + result.affectedRows + date + ';   ';
+//             });
 //
-// weather();
-// forecastopenweathermap();
+//         })
+//         .catch(err => console.log('Error', err))
+// }
 //
+// function openweathermap() {
+//     axios.get('http://api.openweathermap.org/data/2.5/forecast?q=Haifa,il&appid=82d3d463b96e530d4b14b76571e81d3a')
+//         .then(res => {
+//             let result = res.data;
+//
+//             let forecast = result.list.filter(res => {
+//                 let date = moment.unix(res.dt);
+//                 return (date >= moment.utc().endOf('day') && date <= moment.utc().add(1, 'day').endOf('day'))
+//             });
+//
+//             forecast.map(result => {
+//                 let date = result.dt_txt;
+//                 let values = [['Openweathermap forecast', date, result.main.temp - 273.15, result.wind.deg, result.wind.speed, result.wind.gust]];
+//                 con.query(sql, [values], function (err, result, fields) {
+//                     if (err) log += err + ';   ';
+//                     console.log('Openweathermap forecast: ' + result.affectedRows + date);
+//                     log += 'Openweathermap forecast: ' + result.affectedRows + date + ';   ';
+//                 });
+//             });
+//         })
+//         .catch(err => console.log('Error', err))
+// }
+
+
+openweathermap();
+currentweather();
+
 // setInterval(weather, 3600000); //time per 30 min 1800000
 // setInterval(forecastopenweathermap, 86400000);
-//
-// function weather() {
-//     request(currentweather, function (error, res) {
-//         if (error) console.log(error);
-//         let result = JSON.parse(res.body);
-//         let date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
-//         let values = [['Current weather', date, result.main.temp - 273.15, result.wind.deg, result.wind.speed, result.wind.gust]];
-//         con.query(sql, [values], function (err, result, fields) {
-//             if (err) log += err;
-//             console.log('Current weather: ' + result.affectedRows + date);
-//             log += 'Current weather: ' + result.affectedRows + date + ';   ';
-//         });
-//     });
-// }
-//
-// function forecastopenweathermap() {
-//     request(openweathermap, function (error, res) {
-//         if (error) console.log(error);
-//         let result = JSON.parse(res.body);
-//
-//         let forecast = result.list.filter(res => {
-//             let date = moment.unix(res.dt);
-//             return (date >= moment.utc().endOf('day') && date <= moment.utc().add(1,'day').endOf('day'))
-//         });
-//
-//         forecast.map(result => {
-//             let date = result.dt_txt;
-//             let values = [['Openweathermap forecast', date, result.main.temp - 273.15, result.wind.deg, result.wind.speed, result.wind.gust]];
-//             con.query(sql, [values], function (err, result, fields) {
-//                 if (err) log += err + ';   ';
-//                 console.log('Openweathermap forecast: ' + result.affectedRows + date);
-//                 log += 'Openweathermap forecast: ' + result.affectedRows + date + ';   ';
-//             });
-//         });
-//
-//     });
-// }
-
-
 
 // http.createServer(function (req, res) {
 //     res.writeHead(200, {'Content-Type':'text/html'});
@@ -115,9 +82,8 @@ let sql = 'INSERT INTO weather1 (Name, Date, Temperature, WindDirection, WindSpe
 // }).listen(port);
 
 
+
 // test code from template
-
-
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
